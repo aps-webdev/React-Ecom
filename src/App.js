f514437e9145bbda7da1354e95addc2e9e34b67a
studiousTribe <studioustribe@gmail.com>
2021-02-25 00:33:52 +0530

    added global header and link to the home page on logo click

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './container/homepage/homepage.container.jsx';
 import ShopPage from './container/shop/shop.container.jsx';
+import Header from './component/header/header.component';
 
 function App() {
   return (
     <React.Fragment>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
