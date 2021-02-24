1302f485fb9ac450cc68ed52e4853c13db3bb9cf
studiousTribe <studioustribe@gmail.com>
2021-02-24 23:28:49 +0530

    removed non-available logo icon from manifest and updated prop name in menu-item

--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom'; //will give access to history and match prop
 import './menu-item.styles.scss';
 
-function MenuItem({ title, image, size, linkUrl, match, history }) {
+function MenuItem({ title, imageUrl, size, linkUrl, match, history }) {
   return (
     <div
       className={`menu-item ${size}`}
@@ -10,7 +10,7 @@ function MenuItem({ title, image, size, linkUrl, match, history }) {
     >
       <div
         className='background-image'
-        style={{ backgroundImage: `url(${image})` }}
+        style={{ backgroundImage: `url(${imageUrl})` }}
       />
       <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
