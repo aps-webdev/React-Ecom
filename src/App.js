6c19a0173570233089ac03c90877aac3898dbd6c
studiousTribe <studioustribe@gmail.com>
2021-03-02 01:56:17 +0530

    firestore used for the user details to be stroed in firebase and added sign up with email and password

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './container/homepage/homepage.container';
 import ShopPage from './container/shop/shop.container';
 import Header from './component/header/header.component';
 import SignInAndSignUpPage from './container/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -18,9 +18,21 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null;
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/container/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/container/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
 
 import SignIn from '../../component/sign-in/sign-in.component';
+import SignUp from '../../component/sign-up/sign-up.component';
 
 function SignInAndSignUpPage() {
   return (
     <div className='sign-in-and-sign-up'>
       <SignIn />
+      <SignUp />
     </div>
   );
 }
