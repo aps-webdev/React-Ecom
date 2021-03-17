ab09496d88893777ed890926e759af5a9273e828
studiousTribe <studioustribe@gmail.com>
2021-03-18 01:50:14 +0530

    added reselect and updated cart reducer with memoized selectors

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -5,22 +5,27 @@ import './cart-dropdown.styles.scss';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItem } from '../../redux/cart/cart.selectors';
 
 const CartDropdown = ({ cartItems }) => {
   return (
     <div className='cart-dropdown'>
       <div className='cart-items'>
-        {cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))}
+        {cartItems.length === 0 ? (
+          <div className='empty-cart'>Your Shopping Bag is Empty !</div>
+        ) : (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        )}
       </div>
       <CustomButton>go to checkout</CustomButton>
     </div>
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItem(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
